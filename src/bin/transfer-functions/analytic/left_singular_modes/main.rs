9fc832f208c751dc52f24b896f174f6e36ae7e83
Rod Conan <rconan@gmto.org>
2025-07-08 19:28:12 -0700

    LEFT SINGULAR MODES FOR MULTIPLE SINGULAR VALUES

--- src/bin/transfer-functions/analytic/left_singular_modes/main.rs
+++ src/bin/transfer-functions/analytic/left_singular_modes/main.rs
@@ -29,24 +29,46 @@ fn main() -> anyhow::Result<()> {
     // let sys = Sys::from_tarball(&path)?;
     println!("System loaded in {}s", now.elapsed().as_secs());
 
-    let idx = [21, 29, 46, 53, 67];
+    let idx = [
+        (21, vec![0]),
+        (29, vec![0, 1]),
+        (46, vec![0, 1, 2, 3]),
+        (53, vec![0, 1]),
+        (67, vec![0, 1]),
+        (83, vec![0]),
+        (89, vec![0]),
+        (110, vec![0, 1]),
+        (138, vec![0, 1]),
+    ];
+    let idx = [
+        (142, vec![0]),
+        (166, vec![0, 1]),
+        (194, vec![0, 1]),
+        (223, vec![0, 1]),
+        (242, vec![0]),
+        (327, vec![0, 1]),
+        (377, vec![0, 1]),
+        (486, vec![0, 1]),
+    ];
     // println!("Computing the system singular values");
-    // let now = Instant::now();
+    let now = Instant::now();
     let nu = sys.frequencies();
     let mut lsms = vec![];
-    for i in idx.into_iter() {
+    for (i, s_i) in idx.into_iter() {
         let (u, s) = sys[i].left_singular_modes();
         // dbg!(&s[..3]);
-        let u0 = u.column(0);
-        // let u0_nrm: Vec<_> = u0.as_slice().iter().map(|x| x.norm()).collect();
-        lsms.push(LeftSingularMode {
-            nu: nu[i],
-            s: s[0],
-            m: u0.as_slice().to_vec(),
-        });
+        for j in s_i {
+            let u0 = u.column(j);
+            // let u0_nrm: Vec<_> = u0.as_slice().iter().map(|x| x.norm()).collect();
+            lsms.push(LeftSingularMode {
+                nu: nu[i],
+                s: s[j],
+                m: u0.as_slice().to_vec(),
+            });
+        }
     }
     // dbg!(&u0.as_slice()[..10]);
-    // println!("Singular modes computed in {}s", now.elapsed().as_secs());
+    println!("Singular modes computed in {}s", now.elapsed().as_secs());
 
     // let path = Path::new(&repo).join(format!("{}_sv.pkl", root));
     let file = File::create(path.with_extension("lsm.pkl"))?;
